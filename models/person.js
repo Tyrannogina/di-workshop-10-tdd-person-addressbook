7f2d9b447e5d9c70a20a1b136877e0b543f7631d
Sara Latorre <sara.veen@ada.ac.uk>
2020-01-15 12:38:41 +0000

    First draft of Person class and tests

--- models/person.js
+++ models/person.js
@@ -1,1 +1,66 @@
-// Your Person class goes here!
+class Person {
+  constructor(firstName, surname, dob) {
+    this.firstName = this.capitalise(firstName);
+    this.surname = this.capitalise(surname);
+    this.dob = dob;
+    this.emails = [];
+    this.phoneNumbers = [];
+  }
+
+  /**
+   * ensureFirstLetterIsUppercase - description
+   *
+   * @param  {string} word Any word
+   * @return {string} Word with the first letter uppercase
+   */
+  capitalise(word) {
+    let newWord = word;
+
+    if (word[0] !== word[0].toUpperCase()) {
+      newWord = word[0].toUpperCase() + word.substring(1);
+    }
+
+    return newWord;
+  }
+
+  /**
+   * Return full full name
+   * @return {string}
+   */
+  fullName() {
+    return `${this.firstName} ${this.surname}`;
+  }
+
+  /**
+   * Adds email to email list.
+   *
+   * @param {string} email
+   * @return {void}
+   */
+  addEmail(email) {
+    this.emails.push(email);
+  }
+
+
+  /**
+   * Adds phone number to array of phone numbers.
+   *
+   * @param  {string} phoneNumber
+   * @return {void}
+   */
+  addPhone(phoneNumber) {
+    this.phoneNumbers.push(phoneNumber);
+  }
+
+
+  /**
+   * printDetails - Prints the details of the person.
+   *
+   * @return {void}
+   */
+  printDetails() {
+
+  }
+}
+
+module.exports = Person
--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,46 @@
 const chai = require('chai')
 const expect = chai.expect
 
-// Your Person test goes here!
+const Person = require('../models/person.js')
+
+describe('Person', () => {
+  it('should initialise properly', () => {
+    var person = new Person('Eliza', 'Hamilton', 'August 9, 1757');
+
+    expect(person.firstName).to.equal('Eliza');
+    expect(person.surname).to.equal('Hamilton');
+    expect(person.dob).to.equal('August 9, 1757');
+    expect(person.emails).to.deep.equal([]);
+    expect(person.phoneNumbers).to.deep.equal([])
+  })
+
+  it('should capitalise the first name and surname', () => {
+    var person = new Person('eliza', 'hamilton', 'August 9, 1757');
+
+    expect(person.firstName).to.equal('Eliza');
+    expect(person.surname).to.equal('Hamilton');
+  })
+
+  it('should return full name', () => {
+    var person = new Person('Angelica', 'Schuyler', 'February 20, 1756');
+    const fullName = person.fullName();
+
+    expect(fullName).to.equal('Angelica Schuyler');
+  })
+
+  it('should add email into email list', () => {
+    var person = new Person('Angelica', 'Schuyler', 'February 20, 1756');
+    person.addEmail('neverSatisfied@gmail.com');
+    person.addEmail('elizaFan@gmail.com');
+
+    expect(person.emails).to.deep.equal(['neverSatisfied@gmail.com', 'elizaFan@gmail.com']);
+  })
+
+  it('should add phones into phone list', () => {
+    var person = new Person('Angelica', 'Schuyler', 'February 20, 1756');
+    person.addPhone('0687809898687');
+    person.addPhone('3897459734589');
+
+    expect(person.phoneNumbers).to.deep.equal(['0687809898687', '3897459734589']);
+  })
+})
